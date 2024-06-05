96544bd305ab612203e0b3878b4682caeea5781a
Sainivedhana <sainivy08@gmail.com>
2024-06-05 15:51:43 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Sai Nivedhana',
+    author_email= 'sainivy08@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
